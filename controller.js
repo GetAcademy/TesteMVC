bb694933aee3ba0920c596c06b30c0561c2d9243
GET Academy <terje@kolderup.net>
2020-04-27 11:20:23 +0200

    x

--- controller.js
+++ controller.js
@@ -1,5 +1,11 @@
 // controller
 function register() {
     persons.push(personName);
+    personName = '';
+    updateView();
+}
+
+function deletePerson(index) {
+    persons.splice(index, 1);
     updateView();
 }
